4239fe446481b9d030d406d891f062b53f0b156f
Ryan Mack <rymack@cisco.com>
2023-01-13 01:05:39 -0500

    Update robotics section for mobile and make it cleaner

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import { SectionTilesProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
+import FeaturesSplit from './FeaturesSplit';
 import Image from '../elements/Image';
 
 const propTypes = {
@@ -21,7 +21,7 @@ const FeaturesTiles = ({
   invertColor,
   pushLeft,
   ...props
-}) => {
+}) => { 
 
   const outerClasses = classNames(
     'features-tiles section',
@@ -43,11 +43,6 @@ const FeaturesTiles = ({
     pushLeft && 'push-left'
   );
 
-  const sectionHeader = {
-    title: 'FIRST Robotics',
-    paragraph: ''
-  };
-
   return (
     <section
       {...props}
@@ -55,40 +50,12 @@ const FeaturesTiles = ({
     >
       <div className="container">
         <div className={innerClasses}>
-          <SectionHeader data={sectionHeader} className="center-content" />
-          <div className="center-content">
-            <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400"> 
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-            </p>
-          </div>
-
-          {/* What is FIRST? */}
-          <div className="WhatIsFirst">
-            {/* Text, left-aligned */}
-            <div className="splitDivChild" style={{ width:"48%", float:"left", height: "400px" }}>
-              <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200" style={{textAslign:"center"}}>
-                What is FIRST?
-              </h1>
-              <div className="desc">
-                <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400" style={{textAslign:"left", textIndent: "50px"}} > 
-                FIRST® was founded in 1989 by Dean Kamen to inspire young people to pursue education and career opportunities in STEM. Each year, FIRST® Robotics teams, along with the guidance of adult mentors, have six weeks from the first Saturday in January to design, build, and program a robot that is capable of playing a sports-based game. Additionally, aside from its STEM components, students are expected to develop service projects, locate community stakeholders, create a well-defined business plan, market the team, and develop a sustainable program.
-                </p>
-              </div>
-            </div>
-
-            <div className="splitDivChild" style={{ width:"2%", float:"left", height: "450px" }}>
-
-            </div>
-
-            {/* Video, right-aligned */}
-            <div className="splitDivChild" style={{ width:"48%", float:"right", height: "450px"}}>
-              <div style={{height:"75px"}}></div>
-              {/* Video div */}
-              <iframe width="550" height="315" src="https://www.youtube.com/embed/i1QyM9WTF18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-          </div>
+          <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
 
           <div className="center-content">
+            <div style = {{height: "75px"}}>
+
+            </div>
             <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400"> 
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
             </p>
@@ -98,13 +65,11 @@ const FeaturesTiles = ({
             <div className="tiles-item reveal-from-bottom">
               <div className="tiles-item-inner">
                 <div className="features-tiles-item-header">
-                  {/* <div className="features-tiles-item-image mb-16"> */}
                     <Image
                       src={require('./../../assets/images/gear.png')}
                       alt="Features tile icon 01"
                       width={64}
                       height={64} />
-                  {/* </div> */}
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8" id="robotics">
@@ -120,13 +85,11 @@ const FeaturesTiles = ({
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
               <div className="tiles-item-inner">
                 <div className="features-tiles-item-header">
-                  {/* <div className="features-tiles-item-image mb-16"> */}
                     <Image
                       src={require('./../../assets/images/pencil.png')}
                       alt="Features tile icon 02"
                       width={64}
                       height={64} />
-                  {/* </div> */}
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
@@ -142,13 +105,11 @@ const FeaturesTiles = ({
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
               <div className="tiles-item-inner">
                 <div className="features-tiles-item-header">
-                  {/* <div className="features-tiles-item-image mb-16"> */}
                     <Image
                       src={require('./../../assets/images/terminal.png')}
                       alt="Features tile icon 03"
                       width={64}
                       height={64} />
-                  {/* </div> */}
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
@@ -164,13 +125,11 @@ const FeaturesTiles = ({
             <div className="tiles-item reveal-from-bottom">
               <div className="tiles-item-inner">
                 <div className="features-tiles-item-header">
-                  {/* <div className="features-tiles-item-image mb-16"> */}
                     <Image
                       src={require('./../../assets/images/briefcase.png')}
                       alt="Features tile icon 04"
                       width={64}
                       height={64} />
-                  {/* </div> */}
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
@@ -183,39 +142,14 @@ const FeaturesTiles = ({
               </div>
             </div>
 
-            {/* <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                   { <div className="features-tiles-item-image mb-16"> }
-                    <Image
-                      src={require('./../../assets/images/beaker.png')}
-                      alt="Features tile icon 05"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Business
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div> */
-            }
-
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
               <div className="tiles-item-inner">
                 <div className="features-tiles-item-header">
-                  {/* <div className="features-tiles-item-image mb-16"> */}
                     <Image
                       src={require('./../../assets/images/hardhat.png')}
                       alt="Features tile icon 06"
                       width={64}
                       height={64} />
-                  {/* </div> */}
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
@@ -227,11 +161,10 @@ const FeaturesTiles = ({
                 </div>
               </div>
             </div>
-
           </div>
-          <div style={{height:"25px"}}></div>
-          <div className="center-content">
-            <iframe width="760" height="415" src="https://www.youtube.com/embed/DasJsXyBh6A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800" style={{textAlign: "center"}}>
+            <iframe style={{margin:"auto"}} width="560" height="315" src="https://www.youtube.com/embed/DasJsXyBh6A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
         </div>
       </div>

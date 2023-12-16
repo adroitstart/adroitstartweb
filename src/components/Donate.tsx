66ca973348fed4d9a61bb4170e1060abe9d00f96
Ryan Mack <103139195+rymack-te@users.noreply.github.com>
2023-12-15 23:08:17 -0500

    Accomodate small screens for donate box

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -1,7 +1,7 @@
 const Donate = () => {
     return (
       <section className="bg-gray-100" id="donate">
-        <div className="relative h-screen flex items-center justify-center overflow-hidden grid grid-cols-3 content-end bg-[url('/images/slide-7.jpg')] bg-cover">
+        <div className="relative h-screen flex items-center justify-center overflow-hidden grid grid-cols-1 md:grid-cols-3 content-end bg-[url('/images/slide-7.jpg')] bg-cover">
             <div></div>
             <div className="mt-24">
                 <iframe src="https://donorbox.org/embed/adroit-general-fund" name="donorbox" height="900" className="container mx-auto px-4 py-16"></iframe>

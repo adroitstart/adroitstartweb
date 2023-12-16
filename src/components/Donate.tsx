c3f7d363ee81038b1cc6c9ebd7f4c364cfff86f1
Ryan Mack <kamoras95@gmail.com>
2023-12-16 01:10:23 -0500

    Center the donation box

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -1,12 +1,12 @@
 const Donate = () => {
     return (
       <section className="bg-gray-100" id="donate">
-        <div className="relative h-screen flex items-center justify-center overflow-hidden grid grid-cols-1 md:grid-cols-3 content-end bg-[url('/images/slide-7.jpg')] bg-cover">
-            <div></div>
-            <div className="mt-24">
-                <iframe src="https://donorbox.org/embed/adroit-general-fund" name="donorbox" height="900" className="container mx-auto px-4 py-16"></iframe>
+        <div className="container h-screen px-4 mx-auto items-center justify-center bg-[url('/images/slide-7.jpg')] bg-cover">
+            <div className="relative h-screen flex items-center justify-center overflow-hidden align-middle">
+                <div className="">
+                    <iframe src="https://donorbox.org/embed/adroit-general-fund" name="donorbox" height="900"></iframe>
+                </div>
             </div>
-            <div></div>
         </div>
         
       </section>

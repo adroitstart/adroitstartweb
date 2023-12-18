022bfdca6643484095df2f3f84aeb32fb289e885
Ryan Mack <mack.ryanm@gmail.com>
2023-12-17 21:31:27 -0500

    Adjust height and width to be a little more natural

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -1,9 +1,11 @@
 const Donate = () => {
     return (
         <section className="bg-gray-100" id="donate">
-            <div className="flex flex-col h-screen justify-center items-center bg-[url('/images/slide-7.jpg')] bg-cover bg-center">
+            <div style={{ minHeight: '1200px' }} className="flex flex-col h-screen justify-center items-center bg-[url('/images/slide-7.jpg')] bg-cover bg-center">
                 <div className="">
-                    <iframe src="https://donorbox.org/embed/adroit-general-fund" name="donorbox" height="525"></iframe>
+                    <iframe src="https://donorbox.org/embed/adroit-general-fund" name="donorbox" scrolling="no"
+                        style={{ height: '900px', width: '350px' }}
+                        className="w-full h-full rounded-lg flex justify-center items-center"></iframe>
                 </div>
             </div>
         </section>

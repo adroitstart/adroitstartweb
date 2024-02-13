2bae9b62b7de2c5b61c0b8bafb7671aa5e91a660
Ryan Mack <mack.ryanm@gmail.com>
2024-02-12 23:55:31 -0500

    Add programs page

---
+++ src/components/GraphPaper.tsx
@@ -0,0 +1,9 @@
+const GraphPaper = ({ children }: any) => {
+return (
+    <section className="bg-[url('/images/graph-paper.png')] bg-contain bg-center border-hidden">
+        {children}
+    </section>
+)
+}
+
+export default GraphPaper;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { decodeEmail } from '../utils';
 
 const images = [
   '/images/slide-4.jpg',
@@ -7,11 +6,9 @@ const images = [
   '/images/slide-1.jpg',
 ];
 
-const Hero = () => {
+const Hero = ({ children }: any ) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isLoaded, setIsLoaded] = useState(false);
-
-  const benEmail = "YmVubmFkb2xza3lAYWRyb2l0c3RhcnQub3Jn";
   
   useEffect(() => {
     const interval = setInterval(() => {
@@ -49,15 +46,7 @@ const Hero = () => {
         ))}
         <div className="absolute inset-0 flex items-center justify-center z-20">
           <div className="text-center text-white">
-            <p className="font-sans font-bold text-4xl md:text-6xl mb-12">
-              Imagine It. Design It. Build It.
-            </p>
-            <a
-              href={`mailto:${decodeEmail(benEmail)}`}
-              className="bg-white hover:bg-gray-100 text-black font-bold py-2 px-6 rounded-full"
-            >
-              Get Involved
-            </a>
+            {children}
           </div>
         </div>
       </div>

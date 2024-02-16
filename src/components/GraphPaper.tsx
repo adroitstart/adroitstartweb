23dece0270d190cee2299a23308ff112e92cd806
Ryan Mack <mack.ryanm@gmail.com>
2024-02-15 20:57:41 -0500

    Fix background :

--- src/components/GraphPaper.tsx
+++ src/components/GraphPaper.tsx
@@ -1,6 +1,6 @@
 const GraphPaper = ({ children }: any) => {
 return (
-    <section className="bg-[url('/images/graph-paper.png')] bg-cover bg-no-repeat bg-center border-hidden md:bg-contain md:bg-repeat-y">
+    <section className="bg-[url('/images/graph-paper.png')] bg-cover bg-no-repeat bg-center border-hidden md:bg-contain md:bg-repeat">
         {children}
     </section>
 )
